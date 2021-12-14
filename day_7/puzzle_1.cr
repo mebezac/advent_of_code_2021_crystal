0204e9cf0668a077b811a93700663540d437db79
Zac Clay <zacclay@gmail.com>
2021-12-13 20:59:54 -0500

    Day 7 done 🦀

---
+++ day_7/puzzle_1.cr
@@ -0,0 +1,13 @@
+def fuel_cost(input)
+  crab_positions = input.split(",").map { |s| s.to_i }.tally
+  fuel_costs = {} of Int32 => Int32
+  crab_positions.each_key do |move_to|
+    cost = crab_positions.sum do |position, count|
+      (position - move_to).abs * count
+    end
+    fuel_costs[move_to] = cost
+  end
+  fuel_costs.to_a.sort_by { |k, v| v }[0]
+end
+
+puts fuel_cost(File.read(ARGV[0]))
